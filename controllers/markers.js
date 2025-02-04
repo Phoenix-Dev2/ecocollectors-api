22a6ec4fefde5eccc2483851f1b564c112c8291a
Phoenix-Dev <116443234+Phoenix-Dev1@users.noreply.github.com>
2025-02-05 01:48:58 +0200

    [FIX] - #1 - Syntax Error
    fixed syntax error on db export
    
    module.exports = { db };
     instead of:
    module.exports = db ;

--- controllers/markers.js
+++ controllers/markers.js
@@ -1,9 +1,9 @@
-const { db } = require('../db.js');
+const { db } = require("../db.js");
 
 const getActiveMarkers = (req, res) => {
   const q = req.query.type
-    ? 'SELECT * FROM markers WHERE type=? AND active=1'
-    : 'SELECT * FROM markers WHERE active=1';
+    ? "SELECT * FROM markers WHERE type=? AND active=1"
+    : "SELECT * FROM markers WHERE active=1";
   db.query(q, [req.query.type], (err, data) => {
     if (err) return res.send(err);
     return res.status(200).json(data);
--- db.js
+++ db.js
@@ -1,11 +1,17 @@
-const mysql = require("mysql2");
+const mysql2 = require("mysql2");
 
-const db = mysql.createConnection({
+const db = mysql2.createConnection({
   host: "ecocollectors-ecocollectors.e.aivencloud.com",
   port: 13351,
   user: "avnadmin",
   password: "AVNS_oFUMlpjfkhwbxqAxdxu",
   database: "defaultdb",
+  waitForConnections: true,
+  connectionLimit: 50,
+  queueLimit: 0,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 db.connect((err) => {
@@ -16,4 +22,4 @@ db.connect((err) => {
   console.log("Connected to the database!");
 });
 
-module.exports = db;
+module.exports = { db };
