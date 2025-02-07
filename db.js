c6e3b184464e909bf33e4752d722b90648872ded
Phoenix-Dev <116443234+Phoenix-Dev1@users.noreply.github.com>
2025-02-07 17:11:37 +0200

    [FIX] - #1 DB

--- db.js
+++ db.js
@@ -14,12 +14,4 @@ const db = mysql2.createPool({
   },
 });
 
-db.connect((err) => {
-  if (err) {
-    console.error("Database connection failed: ", err.message);
-    return;
-  }
-  console.log("Connected to the database!");
-});
-
 module.exports = db.promise();
